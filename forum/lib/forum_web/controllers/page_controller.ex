984865a941bd77294e33734ac395d89b92841b2c
Natnael1alem <natnaelalem10@gmail.com>
2025-02-17 16:45:00 +0300

    user migrated

--- forum/lib/forum_web/controllers/page_controller.ex
+++ forum/lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,18 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+  
+  def users(conn, _params) do
+    IO.puts("the users controller has been invoked")
+
+    users = [
+      %{id: 1, name: "Alice", email: "alice@gmail.co"},
+      %{id: 2, name: "Joe", email: "joe@gmail.co"},
+      %{id: 3, name: "Mark", email: "mark@gmail.co"},
+      %{id: 4, name: "John", email: "john@gmail.co"},
+    ]
+
+    # render(conn, :users, users: users, layout: false) #only use for non-spa
+    json(conn, users: users) #spa friendly
+  end
 end
